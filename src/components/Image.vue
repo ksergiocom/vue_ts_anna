ce3d726945266af204e415afc2d675e7944ef5b0
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-18 14:48:48 +0100

    Lazy Loading - Estilos

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
+    import {ref} from 'vue'
     import MainLogo from '@/assets/img/main-logo.jpg'
     import { Photo } from '@/types'
 
+    let imgTag = ref<HTMLImageElement>()
 
     const props = defineProps<{
         photo: Photo;
     }>();
 
+
+
 </script>
 
 <template>
     <div>
-        <img v-lazy="{
+        <img ref="imgTag" class="hidden image" v-lazy="{
             src:props.photo.urlPublica,
             loading:MainLogo,
         }">
@@ -20,13 +24,21 @@
 
 <style scoped>
     div{
-        background-color: black;
+        background-color: rgba(0, 0, 0, 0.05);
     }
 
     img{
         height: 100%;
         width: 100%;
         object-fit: cover;
+        transition: all 250ms ease-in-out;
+        overflow: hidden;
     }
+
+    .image[lazy="loading"] {
+        opacity: 0;
+        filter: blur(4px);
+    }
+
 
 </style> 
