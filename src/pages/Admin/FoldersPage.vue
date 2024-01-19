53bcf9f12f696d00814e62117159710a6357bc12
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-19 18:06:54 +0100

    Admin - Servicio folders

--- src/pages/Admin/FoldersPage.vue
+++ src/pages/Admin/FoldersPage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+    import { ref, onMounted } from 'vue';
+    import {StorageService} from '@/services/storage.service'
 
+    let folders = ref([])
+
+    onMounted(async ()=>{
+        await StorageService.getSharedFolders()
+    })
 </script>
 
 <template>
     <div>
         <h3>Folders page</h3>
+        <ul>
+            <li v-for="folder in folders">{{ folder }}</li>
+        </ul>
     </div>
 </template>
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,23 +1,39 @@
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, listAll } from "firebase/storage";
+import {app} from '@/firebase'
+
+const storage = getStorage(app);
+
 
 class StorageService {
-    public static async uploadPublicPhoto(files:FileList){
-        const storage = getStorage()
+    
+    public static async getSharedFolders(){
+        const listRef = ref(storage,'shared')
 
-        const promises = []
-
-        Array.from(files).forEach(file=> {
-            const originalFileName = file.name
-            const photoFileRef = ref(storage, 'public_photos/'+originalFileName)
-            const promise = uploadBytes(photoFileRef, file)
-            console.log(originalFileName)
-
-            promises.push(promise)
-        })
-
-        await Promise.all(files)
-        console.log('Uploaded successfully!')
+        const res = await listAll(listRef)
+        const folders = res.prefixes.map(folder=>folder.name)
+        
+        console.log(folders)
     }
+
+
+    // public static async uploadPublicPhoto(files:FileList){
+    //     const storage = getStorage()
+
+    //     const promises = []
+
+    //     Array.from(files).forEach(file=> {
+    //         const originalFileName = file.name
+    //         const photoFileRef = ref(storage, 'public_photos/'+originalFileName)
+    //         const promise = uploadBytes(photoFileRef, file)
+    //         console.log(originalFileName)
+
+    //         promises.push(promise)
+    //     })
+
+    //     await Promise.all(files)
+    //     console.log('Uploaded successfully!')
+    // }
+
 }
 
 export {StorageService}
