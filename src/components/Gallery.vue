6f49ab6d58c745eb1a0630a0ba45bd0447227a7a
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-19 10:56:32 +0100

    CHECKPOIN

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,100 +1,113 @@
 <script setup lang="ts">
     import Image from '@/components/Image.vue'
-    import {ref, onMounted, computed} from 'vue'
+    import {ref, onMounted} from 'vue'
     import {PhotosService} from '@/services/photos.service'
     import { Photo } from '@/types'
 
-    const photos = ref<Photo[]>([])
-    /*
-      Romper la simetría.
+    // Necesario para este componente solo.
+    interface PhotoWithCSSClass extends Photo {
+        cssClass:'v'|'h'|'h2'
+    }
 
-      Para romper la simetría inicial los dos primeros elementos tiene que tener altos diferentes.
-      Esto lo consigo usando una imagen vertical y una horizontal pequeña, o viceversa.
-      No utilizo la vertical y la horizontal grande porque tienen el mismo alto, por lo que quedan rectas.
-
-      Ahora lo importante es buscar la primera imagen vertical y la primera horizontal
-      y colocarlas como las dos primeras para poder crear este efecto.
-    */
+    const photos = ref<PhotoWithCSSClass[]>([])
 
     onMounted(async () => {  
-        // Deben ser multiplos de 4 para evitar huecos en el grid
-        const photosJSON = await PhotosService.getPublicPhotos(4*20)
+        // SOLO PARA VALORES PARES!
+        const photosJSON = await PhotosService.getPublicPhotos(20)
 
-        // Busco la primer foto horizontal y la primer vertical
-        const idxPhotoHorizontal = photos.value.findIndex(photo=>photo.orientacion=='horizontal')
-        const photoHorizontal = photos.value.splice(idxPhotoHorizontal,1)
-
-        const idxPhotoVertical = photos.value.findIndex(photo=>photo.orientacion=='vertical')
-        const photoVertical = photos.value.splice(idxPhotoVertical,1)
-
-        /*
-          Inserto las dos fotos en las dos primeras posiciones, en orden "aleatorio".
-        */
-        if(Math.random()>0.5){
-            // El spread operator es por haber usado el splice arriba...
-            photosJSON.unshift(...photoHorizontal)
-            photosJSON.unshift(...photoVertical)
-        }else{
-            photosJSON.unshift(...photoVertical)
-            photosJSON.unshift(...photoHorizontal)
-        }
-
-        photos.value = photosJSON
-
-    })
-
-    // Asignando las clases de CSS
-    const computedPhotos = computed(()=>{
-
-        // Voy a usar un array que almacena las dos ultimas clases aplicadas.
-        const ultimasClasses:Array<'v'|'v2'|'h'|'h2'> = []
-
-        const photosWithClasses = photos.value.map((photo,idx)=>{
-            const {orientacion} = photo
-
-            // TS me obliga a declarar un default. Si no da error abajo.
-            let cssClass:'v'|'v2'|'h'|'h2' = 'h'
-
-            if(orientacion == 'vertical'){
-                cssClass = 'v'
-                /*
-                  Si las dos ultimas clases usadas son la misma cambiarla por 
-                  una horizontal. Aunque la imagen sea vertical. Da igual.
-                */
-                if(ultimasClasses.length>1 && ultimasClasses.every(el=>el==='v')){
-                    cssClass = 'h'
-                }
-            }
-            if(orientacion == 'horizontal'){
-                
-                cssClass = Math.random() > 0.5 ? 'h2' : 'h'
-                /*
-                    El primer y segundo elemento nunca puede ser uno grande.
-                    Porque como va acompañado con un elemento vertical, no crearía 
-                    la asimetría que buscamos. (Se quedarían como a la misma altura)
-                */
-                if(idx==0 || idx==1){
-                   cssClass = 'h'
-                }
-
-                // Si la clase seleccionada es la misma que las dos ultimas, cambiarla por la otra
-                if(ultimasClasses.length>1 && ultimasClasses.every(el=>el==='h')){
-                    cssClass = cssClass == 'h' ? 'h2' : 'h'
-                }
-            }
-            
-            ultimasClasses.push(cssClass)
-            if(ultimasClasses.length>2){
-                ultimasClasses.shift()
-            }
-            
-            return{
-                ...photo,
-                cssClass,
+        //Para que me cuadren todas. La superficie total sumada debe ser multiplo de 6.  
+        let v = 0
+        let h = 0
+        
+        photosJSON.forEach(photo=>{
+            switch (photo.orientacion){
+                case 'vertical':
+                    v++
+                    break
+                default:
+                    h++
             }
         })
 
-        return photosWithClasses
+        // Calculando superficies
+        const sTotalMin = v*2+h
+        const sTotalMax = v*2+h*4
+
+        const sTotalMedia = Math.floor((sTotalMin+sTotalMax)/2)
+        // Buscar el siguiente multiplo de 6 a partir de la media
+        const sTotalMediaSiguienteMultiploDe6 = (Math.floor((sTotalMedia)/6)+1)*6
+
+        // Estas no dependen de nada. Son tal cual.
+        const sParaVerticales = v*2
+
+        // Superficie total disponible para las horizontales
+        let sParaHorizontales = sTotalMediaSiguienteMultiploDe6 - sParaVerticales
+        // Aqui hay truco. A veces no hay horizontales suficientes
+        // Sacamos la cantidad total y comprobamos si existen h pequeñas + h grandes suficientes.
+        // Si no es así agregar una H2 y restar 4 H y volver a comprobar. etc.
+        
+        // Porcentaje en cual aparecen las grandes respecto a las pequeñas por defecto. PARA AJUSTAR EL MINIMO QUE APARECEN.
+        const PORCENTAJE_PROPORCION = 1/3
+        let sParaH2 = PORCENTAJE_PROPORCION * sParaHorizontales
+
+        let cantidadH2 = Math.floor(sParaH2/4)
+        // Ahora como se trunca. Tenemos que buscar cuanta superficie real nos queda para las pequeñas
+        let cantidadH = sParaHorizontales - (cantidadH2*4)
+
+        // Comprobar si la cantidad calculada es MAYOR que h(cantidad disponible que viene del server)
+        while(cantidadH+cantidadH2 > h+1 ){
+            cantidadH2++
+            cantidadH = cantidadH - 4
+        }
+
+        console.table({
+            h,
+            v,
+            sParaHorizontales,
+            sParaVerticales,
+            sTotalMediaSiguienteMultiploDe6,
+            sParaV:v*2,
+            sParaH:cantidadH,
+            sParaH2Usada:cantidadH2*4,
+            sRestante:sTotalMediaSiguienteMultiploDe6-cantidadH2*4-cantidadH-sParaVerticales,
+            cantidadH,
+            cantidadH2,
+            cantidadV:v,
+            modulo6V:sParaVerticales%6,
+            modulo6H:sParaHorizontales%6,
+        })
+
+        let cantidadHGrandesDisponibles = cantidadH2
+        let cantidadHPequeñasDisponibles = cantidadH
+        const photosWithClasses = photosJSON.map((photo):PhotoWithCSSClass=>{
+            if(photo.orientacion=='vertical'){
+                return {
+                    ...photo,
+                    cssClass:'v',
+                }
+            }
+            if(photo.orientacion=='horizontal'){
+                if(cantidadHGrandesDisponibles){
+                    cantidadHGrandesDisponibles--
+                    return {
+                    ...photo,
+                    cssClass:'h2',
+                    }
+                }else{
+                    cantidadHPequeñasDisponibles--
+                    return {
+                    ...photo,
+                    cssClass:'h',
+                    }
+                }
+            }
+            return {
+                ...photo,
+                cssClass:'h'
+            }
+        })
+
+        photos.value = photosWithClasses
     })
 
 
@@ -103,7 +116,7 @@
 
 <template>
     <section>
-        <div v-for="photo in computedPhotos" :class="['celda',photo.cssClass]" :key="photo.id">
+        <div v-for="photo in photos" :class="['celda',photo.cssClass]" :key="photo.id">
             <Image :photo="photo"/>
         </div>
     </section>
@@ -117,8 +130,6 @@
         grid-template-columns: repeat(2, 1fr); /* Dos columnas de igual ancho */
         grid-auto-rows: calc(0.67 * (100vw / 2)); 
 
-        /* grid-template-columns: repeat(auto-fit, 200px);
-        grid-auto-rows: 120px; */
         grid-auto-flow: dense;
         gap: 5px;
         justify-content: center;
@@ -145,19 +156,13 @@
         grid-row: span 2;
     }
 
-
-@media (min-width:800px) {
+@media (min-width:900px) {
     section{
         grid-template-columns: repeat(3, 1fr); /* Dos columnas de igual ancho */
         grid-auto-rows: calc(0.67 * (100vw / 3)); 
     }
-}
 
-@media (min-width:1200px) {
-    section{
-        grid-template-columns: repeat(4, 1fr); /* Dos columnas de igual ancho */
-        grid-auto-rows: calc(0.67 * (100vw / 3)); 
-    }
+
 }
 
 </style>
