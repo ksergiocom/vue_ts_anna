22872ab757de3d7e0d042595b87a1c25cdc18c76
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-23 14:37:16 +0100

    Hero Parallax

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -14,6 +14,20 @@
 
 <template>
     <main>
+        <v-parallax
+        id="hero"
+        class="mb-10"
+        src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+        >
+            <div class="d-flex flex-column fill-height justify-center align-center text-white">
+            <h1 class="text-h4 font-weight-thin mb-4">
+                Vue, Firebase y TS
+            </h1>
+            <p class="text-center w-1/2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia nihil quo error est dolores qui, tempore ratione esse voluptatem sequi, laborum incidunt ea quaerat vero cupiditate doloremque. Est, officia.
+            </p>
+            </div>
+        </v-parallax>
         <Gallery :photos="photos"/>
     </main>
 </template>
@@ -25,5 +39,9 @@
             width: 60%;
             margin: auto;
         }
+    }
+
+    #hero{
+        height: 100vh;
     }
 </style>
