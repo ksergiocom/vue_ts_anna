d191312cedfed6468be405efb17ac39b6241aced
ksergio <113699828+ksergiocom@users.noreply.github.com>
2024-01-24 14:48:52 +0100

    firebase.config.ts
    Ahora el config esta en un archivo TS separado que no se sube al repositorio

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { collection, getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA7oL3AF-w5WqgFfbz32H1kk278Y-NfzWI",
-  authDomain: "jam-anna.firebaseapp.com",
-  projectId: "jam-anna",
-  storageBucket: "jam-anna.appspot.com",
-  messagingSenderId: "5307014907",
-  appId: "1:5307014907:web:1d8c6416b9da60ac43da5e"
-};
-
+import {firebaseConfig} from '@/firebase.config'
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
